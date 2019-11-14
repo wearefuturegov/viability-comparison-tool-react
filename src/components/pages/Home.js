cd45124ba841b46fc85cd1dee51a24f769560b0c
Chris Evans <evanschris@users.noreply.github.com>
2019-11-14 09:58:42 +0000

    tidy home vars

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -13,45 +13,51 @@ const MapContainer = styled.div`
 `
 
 const Home = () => {
+    // Map & list variables
     const [activeMarker, toggleActiveMarker] = useState(0);
     const [markersData, setMarkersData] = useState([]);
     const [loading, setLoading] =  useState(true);
     const [hoverMarker, toggleHoverMarker] = useState(0);
     const [hasError, setErrors] =  useState(false)
     const API = 'https://viability-comparison-api.herokuapp.com/viability_appraisals'
+    const [filters, setFilters] = useState('?');
+    
+    useEffect(() => {
+        fetchData(API, filters, setMarkersData, setLoading, setErrors)
+    }, [filters]);
 
+
+    // Habitable Filter variables
     const [minHabitableURL, setMinHabitableURL] = useQueryParam('min_habitable_rooms', NumberParam);
     const [maxHabitableURL, setMaxHabitableURL] = useQueryParam('max_habitable_rooms', NumberParam);
     const [minHabitable, setMinHabitable] = useState((minHabitableURL ? minHabitableURL : 0));
     const [maxHabitable, setMaxHabitable] = useState((maxHabitableURL ? maxHabitableURL : 0));    
     const [habitableIsFiltered, setHabitableIsFiltered] = useState(false);
     const [maxTotalRooms, setMaxTotalRooms] = useState(0);
-
-    const [filters, setFilters] = useState('?');
-
-    useEffect(() => {
-        fetchData(API, filters, setMarkersData, setLoading, setErrors)
-    }, [filters]);
+    
 
     return(
         <>
             <FilterBar 
                 toggleActiveMarker={toggleActiveMarker}
                 setFilters={setFilters}
-                minHabitable={minHabitable}
-                setMinHabitable={setMinHabitable}
-                setMinHabitableURL={setMinHabitableURL}
-                maxHabitable={maxHabitable}
-                setMaxHabitableURL={setMaxHabitableURL}
-                setMaxHabitable={setMaxHabitable}
+
+                // habitable filters
                 habitableIsFiltered={habitableIsFiltered}
                 setHabitableIsFiltered={setHabitableIsFiltered}
-                maxTotalRooms={maxTotalRooms}
+                minHabitable={minHabitable}
+                setMinHabitable={setMinHabitable}
                 minHabitableURL={minHabitableURL}
+                setMinHabitableURL={setMinHabitableURL}
+                maxTotalRooms={maxTotalRooms}
+                maxHabitable={maxHabitable}
+                setMaxHabitable={setMaxHabitable}
                 maxHabitableURL={maxHabitableURL}
+                setMaxHabitableURL={setMaxHabitableURL}
             />
             <MapContainer>
                 <List 
+                    // map & list filters
                     loading={loading} 
                     markersData={markersData} 
                     activeMarker={activeMarker} 
@@ -59,12 +65,14 @@ const Home = () => {
                     hoverMarker={hoverMarker} 
                     toggleHoverMarker={toggleHoverMarker}
                     hasError={hasError}
+
+                    // habitable filters
                     setHabitableIsFiltered={setHabitableIsFiltered}
+                    minHabitableURL={minHabitableURL}
                     maxTotalRooms={maxTotalRooms}
-                    setMaxTotalRooms={setMaxTotalRooms}
                     maxHabitable={maxHabitable}
                     setMaxHabitable={setMaxHabitable}
-                    minHabitableURL={minHabitableURL}
+                    setMaxTotalRooms={setMaxTotalRooms}
                     maxHabitableURL={maxHabitableURL}
                 />
 
