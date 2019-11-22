833008c9b65888dee85d820d41e1688a75b9cddf
Chris Evans <evanschris@users.noreply.github.com>
2019-11-22 17:35:56 +0000

    add title

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,13 +6,25 @@ const StyledHeader = styled.div`
     background: #046F9E;
     height: 51px;
     z-index: 999;
+    padding-left: 15px;
+
+    h1 {
+        text-transform: uppercase;
+        color: #fff;
+        margin: 0;
+        line-height: 51px;
+        font-size: 18px;
+        letter-spacing: 0.75px;
+    }
 `
 
 
 export class Header extends Component {
     render() {
         return (
-            <StyledHeader>{this.props.children}</StyledHeader>
+            <StyledHeader>
+                <h1>Viability Compare Tool</h1>
+            </StyledHeader>
         )
     }
 }
