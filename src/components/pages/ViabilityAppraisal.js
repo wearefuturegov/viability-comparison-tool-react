b67378553157c05882d834ed2845ec72990982cc
userman123 <dominic.freeman123@gmail.com>
2019-11-29 12:29:13 +0000

    Add commas to show page - TODO: Move addCommas fn somewhere accessible to both to avoid duplication

--- src/components/pages/ViabilityAppraisal.js
+++ src/components/pages/ViabilityAppraisal.js
@@ -18,7 +18,11 @@ const ViabilityAppraisal = () => {
     useEffect(() => {
         fetchData("https://viability-comparison-api.herokuapp.com/", window.location.pathname, setChosenDevelopment, setLoading, setErrors)
     }, []);
-    
+
+    // TODO: Move this function somewhere so that it is accessible by list and in here to avoid duplication
+    function addCommas(val) {
+      return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    }
     return (
         <SingleViability>
             <BackLink />
@@ -32,20 +36,20 @@ const ViabilityAppraisal = () => {
                     <ul>
                         <li>Local Authority: {chosenDevelopment.attributes.local_authority}</li>
                         <li>Date submitted: {chosenDevelopment.attributes.date_submitted}</li>
-                        <li>GDV: £{chosenDevelopment.attributes.gross_development_value_pence/100}</li>
-                        <li>Construction Costs: £{chosenDevelopment.attributes.construction_costs_pence/100}</li>
+                        <li>GDV: £{addCommas(chosenDevelopment.attributes.gross_development_value_pence/100)}</li>
+                        <li>Construction Costs: £{addCommas(chosenDevelopment.attributes.construction_costs_pence/100)}</li>
                         <li>Storeys: {chosenDevelopment.attributes.stories}</li>
-                        <li>Professional Fees: £{chosenDevelopment.attributes.professional_fees_pence/100}</li>
+                        <li>Professional Fees: £{addCommas(chosenDevelopment.attributes.professional_fees_pence/100)}</li>
                         <li>Professional Fees (% of construction costs): {chosenDevelopment.attributes.professional_fees_as_percentage_of_construction_costs}%</li>
-                        <li>Marketing and letting Fees: £{chosenDevelopment.attributes.marketing_and_letting_pence/100}</li>
+                        <li>Marketing and letting Fees: £{addCommas(chosenDevelopment.attributes.marketing_and_letting_pence/100)}</li>
                         <li>Marketing and letting Fees (% of construction costs): {chosenDevelopment.attributes.marketing_and_letting_as_percentage_of_construction_costs}%</li>
                         <li>Finance (% of GDV): {chosenDevelopment.attributes.finance_as_percentage_of_gdv}%</li>
                         <li>Finance: £{chosenDevelopment.attributes.finance_pence/100}</li>
-                        <li>Financial planning obligations: £{chosenDevelopment.attributes.financial_planning_obligations_pence/100}</li>
+                        <li>Financial planning obligations: £{addCommas(chosenDevelopment.attributes.financial_planning_obligations_pence/100)}</li>
                        
                         <li>Developer Profit (% of GDV): {chosenDevelopment.attributes.developer_profit_as_percentage_of_gdv}%</li>
-                        <li>Residual Land Value: £{chosenDevelopment.attributes.residual_land_value_pence/100}</li>
-                        <li>Benchmark Land Value: £{chosenDevelopment.attributes.benchmark_land_value_pence/100}</li>
+                        <li>Residual Land Value: £{addCommas(chosenDevelopment.attributes.residual_land_value_pence/100)}</li>
+                        <li>Benchmark Land Value: £{addCommas(chosenDevelopment.attributes.benchmark_land_value_pence/100)}</li>
                         <li>Residential Units: {chosenDevelopment.attributes.residential_units}</li>
                         <li>Habitable Rooms: {chosenDevelopment.attributes.habitable_rooms}</li>
                         <li>Commercial Area: {chosenDevelopment.attributes.commercial_area_square_centimetres/100}m&sup2;</li>
